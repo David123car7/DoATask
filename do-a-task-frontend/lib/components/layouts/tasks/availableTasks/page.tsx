dbc784059bb038b1881e7e52c16dda69b6001f28
CrZ <david123car7@gmail.com>
2025-06-02 11:51:03 +0100

    Revert "Tasks Update"
    This reverts commit d6899ee3d708bc3f30566d1ec0d7f01c420305dc.

--- do-a-task-frontend/lib/components/layouts/tasks/availableTasks/page.tsx
+++ do-a-task-frontend/lib/components/layouts/tasks/availableTasks/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 import React from "react";
-import styles from "./page.module.css";
+import styles from './page.module.css'
 import { useState } from "react";
 import { GetNameCommunitySchemaArray } from "@/lib/schemas/community/get-communityName-schema";
 import { GetTasksBeDone } from "@/lib/api/tasks/get.all.taskBeDoneCommunity";
@@ -9,87 +9,71 @@ import { FaLocationPin, FaCoins } from "@/lib/icons/index";
 import { AssignTaskButton } from "../buttons/assign.task.button";
 import { GetUser } from "@/lib/utils/supabase/user/get-user";
 
-export function AvaiableTasks({
-  community,
-}: {
-  community: GetNameCommunitySchemaArray | null;
-}) {
-  const [selectedCommunity, setSelectedCommunity] = useState("");
-  const [tasks, setTasks] = useState<any[]>([]);
-  const [memberTasks, setMemberTasks] = useState<any[]>([]);
 
-  const handleSelectChange = async (
-    e: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const communityName = e.target.value;
-    setSelectedCommunity(communityName);
-    console.log(communityName);
+export function AvaiableTasks({community }: {community: GetNameCommunitySchemaArray | null }){
+
+    const [selectedCommunity, setSelectedCommunity] = useState('');
+    const [tasks, setTasks] = useState<any[]>([]);
+    const [memberTasks, setMemberTasks] = useState<any[]>([]);
+
+    const handleSelectChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const communityName = e.target.value;
+        setSelectedCommunity(communityName);
+        console.log(communityName);
     try {
-      const result = await GetTasksBeDone(communityName);
-      const validateTasks = taskResponseSchema.parse(result);
+        const result = await GetTasksBeDone(communityName)
+        const validateTasks = taskResponseSchema.parse(result);
 
-      setTasks(validateTasks.tasks);
-      setMemberTasks(validateTasks.memberTasks);
+        setTasks(validateTasks.tasks);
+        setMemberTasks(validateTasks.memberTasks);
     } catch (error) {
-      console.error("Erro ao procurar tarefas:", error);
+        console.error('Erro ao buscar tarefas:', error);
     }
-  };
-  console.log("Tarefas", tasks);
+    }
+    console.log("Tarefas", tasks)
 
-  return (
-    <div className={styles.container}>
-      <select
-        onChange={handleSelectChange}
-        className={styles.selectCustom}
-        value={selectedCommunity}
-      >
-        <option value="" disabled>
-          Seleciona Uma Comunidade
-        </option>
-        {(community ?? []).map((c, index) => (
-          <option key={index} value={c.communityName}>
-            {c.communityName}
-          </option>
-        ))}
-      </select>
-      <div className={styles.taskGrid}>
-        {tasks.length > 0 ? (
-          tasks.map((tasks, index) => (
-            <div key={index} className={styles.task}>
-              <div className={styles.imageTask}>
-                <img
-                  src={tasks.imageUrl.signedUrl}
-                  alt={tasks.name}
-                  className={styles.imageTask}
-                />
-              </div>
-              <div className={styles.title}>{tasks.title}</div>
-              <p className={styles.description}>
-                <strong>Descrição: </strong>
-                {tasks.description}
-              </p>
-              <p className={styles.location}>
-                <strong>Localização: </strong>
-                {tasks.location}
-              </p>
-              <p className={styles.description}>
-                <strong>Dificuldade: </strong> {tasks.difficulty}
-              </p>
-              <div className={styles.difficulty}>
-                Recompensas:
-                <p>- {tasks.coins}Moedas</p>
-                <p>- {tasks.points} Pontos</p>
-              </div>
-
-              <div className={styles.buttonContainer}>
-                <AssignTaskButton taskId={tasks.id} />
-              </div>
+    
+    return(
+        <div className={styles.container}>
+            <select
+                onChange={handleSelectChange}
+                className={styles.selectCustom}
+                value={selectedCommunity} 
+                >
+                <option value="" disabled>Selecione Uma Comunidade</option>
+                {(community ?? []).map((c, index) => (
+                    <option key={index} value={c.communityName}>
+                    {c.communityName}
+                    </option>
+                ))}
+                </select>
+            <div className={styles.taskGrid}>
+            
+            {tasks.length > 0 ?(
+                tasks.map((tasks, index) =>(
+                    <div key={index} className={styles.task}>
+                    <div className={styles.imageTask}>
+                        <img src={tasks.imageUrl.signedUrl} alt={tasks.name} className={styles.imageTask}/>
+                    </div>
+                    <div className={styles.title}>{tasks.title}</div>
+                    <p className={styles.description}><strong>Descrição: </strong>{tasks.description}</p>
+                    <p className={styles.location}><strong>Localização: </strong>{tasks.location}</p>
+                        <p className={styles.description}><strong>Dificuldade: </strong> {tasks.difficulty}</p>
+                        <div className={styles.difficulty}>Recompensas:
+                        <p>- {tasks.coins}Moedas</p>
+                        <p>- {tasks.points} Pontos</p>
+                        </div>
+                    
+                    <div className={styles.buttonContainer}>
+                        <AssignTaskButton taskId={tasks.id}/>
+                    </div>
+                </div>
+                ))
+                
+            ):(
+                <h1></h1>
+            )}    
             </div>
-          ))
-        ) : (
-          <h1></h1>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
