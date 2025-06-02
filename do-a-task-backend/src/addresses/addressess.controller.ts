fc422bc04a012e6f02bab441ee603253ac88cfc5
CrZ <david123car7@gmail.com>
2025-06-02 11:22:11 +0100

    Revert "vasco"
    This reverts commit 28646a764cb29c93d8e1a28e90646776496e725e.

--- do-a-task-backend/src/addresses/addressess.controller.ts
+++ do-a-task-backend/src/addresses/addressess.controller.ts
@@ -1,11 +1,9 @@
-import { Body, Controller, Post, Get, UseGuards, Req, Res, Delete, Put, Param } from "@nestjs/common";
+import { Body, Controller, Post, Get, UseGuards, Req, Res } from "@nestjs/common";
 import { AddressService } from "./addresses.service";
 import { CreateAddressDto } from "./dto/addresses.dto";
 import { JwtAuthGuard } from "../auth/guard/jwt.auth.guard";
 import { RequestWithUser } from "../auth/types/jwt-payload.type";
 import { Response } from 'express';
-import {UpdateAddressDto} from "./dto/update.address.dto"
-
 
 @Controller("addresses")
 export class AddressesController {
@@ -31,28 +29,5 @@ export class AddressesController {
     async VefifyAdressses(@Body() body: { minPostalCode: string; maxPostalCode: string }, @Res() res: Response, @Req() req: RequestWithUser){
         const data = await this.addressesService.VefifyAdressses(req.user.sub,body.minPostalCode, body.maxPostalCode)
         return res.json({message: "Address Verified", address: data})
-    }
-
-    @Delete(":id")
-    @UseGuards(JwtAuthGuard)
-    async deleteAddress(
-        @Param('id') id: number,
-        @Req() req: RequestWithUser,
-        @Res() res: Response
-    ) {
-        await this.addressesService.deleteAddress(id, req.user.sub);
-        return res.json({ message: "Address deleted successfully" });
-    }
-
-    @Put(":id")
-    @UseGuards(JwtAuthGuard)
-    async updateAddress(
-        @Param('id') id: number,
-        @Body() dto: UpdateAddressDto,
-        @Req() req: RequestWithUser,
-        @Res() res: Response
-    ) {
-        const updatedAddress = await this.addressesService.updateAddress(id, dto, req.user.sub);
-        return res.json({ message: "Address updated", address: updatedAddress });
     }
 }
